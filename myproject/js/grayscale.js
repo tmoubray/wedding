0c7a1c9bac1deb9e69d0e009bb50cbf19bd2960c
Todd Moubray <toddmoubray@Todds-MacBook-Pro.local>
2017-01-03 15:35:31 -0500

    no message

--- myproject/js/grayscale.js
+++ myproject/js/grayscale.js
@@ -37,7 +37,7 @@ var map = null;
 // When the window has finished loading create our google map below
 google.maps.event.addDomListener(window, 'load', init);
 google.maps.event.addDomListener(window, 'resize', function() {
-    map.setCenter(new google.maps.LatLng(40.6700, -73.9400));
+    map.setCenter(new google.maps.LatLng(39.5100,-84.7461));
 });
 
 function init() {
